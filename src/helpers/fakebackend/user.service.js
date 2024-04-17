d9e4a4d5134d3d4b8e0adb1c5f9315bbb5872cc6
ARNOLD KEVIN VACA PASTOR <avacap2@int.upao.edu.pe>
2024-04-17 11:35:31 -0500

    login2

--- src/helpers/fakebackend/user.service.js
+++ src/helpers/fakebackend/user.service.js
@@ -17,7 +17,7 @@ function login(username, password) {
     };
 
     //return fetch(`http://comfica_back.test:8084/api/login`, requestOptions)
-    return fetch(this.$apiURL + "login", requestOptions)
+    return fetch(`https://back.comficaconect.com/api/login`, requestOptions)
         .then(handleResponse)
         .then(user => {
             if (user.token) {
